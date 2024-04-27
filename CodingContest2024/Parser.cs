7c89b9ed3765c97a195a0b81f59c77251c28920f
ionas_999 <jonas.hinterdorfer@ionas999.at>
2024-04-27 18:59:50 +0200

    ReAdded the Infoline Feature

--- CodingContest2024/Parser.cs
+++ CodingContest2024/Parser.cs
@@ -1,19 +1,19 @@
-﻿namespace CodingContest2024;
-
-public class Parser
+﻿public class Parser
 {
     private readonly string _inputFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Input");
-    private int _currentIndex;
+    private int _currentIndex = 0;
+    private readonly int _infoLinesStart;
 
     private List<Func<List<string>, object>> Parsers { get; }
 
-    public Parser(List<Func<List<string>, object>> parsers, bool isTest = false)
+    public Parser(List<Func<List<string>, object>> parsers, bool isTest = false, int infoLinesStart = 0)
     {
         if (isTest)
         {
             _inputFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Test_Input");
         }
 
+        _infoLinesStart = infoLinesStart;
         Parsers = parsers;
     }
 
@@ -22,11 +22,21 @@ public class Parser
         List<object> parsed = new();
         var input = ReadInputFile().GetEnumerator();
 
+        List<string> infoLines = new();
+        for (int i = 0; i < _infoLinesStart && input.MoveNext(); i++)
+        {
+            infoLines.Add(input.Current);
+        }
+
         while (input.MoveNext())
         {
             foreach (Func<List<string>, object> parsing in Parsers)
             {
                 List<string> lines = ProcessLines(input);
+                if (Parsers.IndexOf(parsing) == 0)
+                {
+                    lines.InsertRange(0, infoLines);
+                }
                 parsed.Add(parsing.Invoke(lines));
             }
         }
